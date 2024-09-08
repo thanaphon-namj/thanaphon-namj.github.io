bc01741e8d9252053a6fb1d6f23946b4bc7ec782
Thanaphon Namjittong <thanaphon.namj@bumail.net>
2024-09-08 17:30:21 +0700

    Add About section

---
+++ src/Sections/About.jsx
@@ -0,0 +1,14 @@
+function About() {
+  return (
+    <section>
+      <h2 className="font-bold text-xl">About</h2>
+      <div className="text-sm">
+        I am a third-year student (transfer student) in the Information
+        Technology at School of Information Technology and Innovation at Bangkok
+        University, and I am very passionate about programming.
+      </div>
+    </section>
+  );
+}
+
+export default About;
