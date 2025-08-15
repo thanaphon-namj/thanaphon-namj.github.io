9d3a412f4ec8a01438fba8f97a9bdccf6a515c91
Thanaphon Namjittong <127986055+thanaphon-namj@users.noreply.github.com>
2025-08-15 20:07:47 +0700

    Update Intro.jsx

--- src/Sections/Intro.jsx
+++ src/Sections/Intro.jsx
@@ -9,7 +9,7 @@ function Intro() {
             Hi, I&apos;m Thanaphon Namjittong 👋
           </div>
           <div className="md:text-xl">
-            Software Engineer, Full Stack Developer, Mobile Developer
+            Software Engineer, Full Stack Developer
           </div>
         </div>
         <div className="flex size-28 rounded-full overflow-hidden">
