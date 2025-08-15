c4ef7d61dd2ec43affadebad16816f7f501df888
Thanaphon Namjittong <127986055+thanaphon-namj@users.noreply.github.com>
2025-08-15 20:07:26 +0700

    Update About.jsx

--- src/Sections/About.jsx
+++ src/Sections/About.jsx
@@ -3,16 +3,13 @@ function About() {
     <section>
       <h2 className="font-bold text-xl">About</h2>
       <div className="text-sm">
-        - Full Stack Developer with 7+ years of experience in web and software
-        development across various industries.
+        - Full Stack Developer with 6+ years of experience in web and software development across various industries.
         <br />
-        - Strong expertise in JavaScript and modern web technologies, capable of
-        delivering both frontend and backend solutions.
+        - Strong expertise in JavaScript and modern web technologies, capable of delivering both frontend and backend solutions.
         <br />
-        - Quick learner with a passion for new technologies and continuous
-        improvement.
-        <br />- Experienced in Agile environments, with a focus on writing clean
-        code, effective debugging, and optimizing system performance.
+        - Quick learner with a passion for new technologies and continuous improvement.
+        <br />
+        - Experienced in Agile environments, with a focus on writing clean code, effective debugging, and optimizing system performance.
       </div>
     </section>
   );
